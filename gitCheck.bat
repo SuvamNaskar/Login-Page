00c21ef680637b1621deed02b452ebc45e285c81
Suvam Naskar <suvamlive@gmail.com>
2024-04-08 17:39:56 +0530

    sync speed 5 sec

--- gitCheck.bat
+++ gitCheck.bat
@@ -1,20 +1,13 @@
 @echo off
 TITLE Checking git
 :a
-ECHO ============================
-
-ECHO NETWORK INFO
-
-ECHO ============================
 
 ipconfig
 
 git fetch
 
-timeout -t 5
-
 git pull
 
-timeout -t 10
+timeout -t 5
 
 goto a
